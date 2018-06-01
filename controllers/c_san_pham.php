6f30312995df831699eb3b671f28281ef9fed46f
dangnghia1997 <dangnghia25197@gmail.com>
2018-06-02 00:12:33 +0700

    cap nhat 2/6 12:12AM

--- controllers/c_san_pham.php
+++ controllers/c_san_pham.php
@@ -10,20 +10,20 @@ class C_san_pham
 		
 		include("models/menudacap.php");
 		
-<<<<<<< HEAD
+
 		$ds_tat_ca_san_pham="";
 		
 		include("models/m_san_pham.php");
 		$m_san_pham=new M_san_pham();
 			 
-=======
->>>>>>> 527178d2f96c5ca383bcd0e38eed2e6ac7a58faf
+
+
 		if(isset($_GET['ma_loai']))
 		{	
 			 $ma_loai=$_GET['ma_loai'];
 			 $loai_san_pham=$m_loai_san_pham->Doc_loai_san_pham_theo_ma_loai($ma_loai);
 			 $danh_muc_hien_tai=$loai_san_pham->ten_loai;
-<<<<<<< HEAD
+
 			 
 			 //sản phẩm
 			 $ds_san_pham_theo_ma_loai=$m_san_pham->Doc_san_pham_theo_ma_loai($ma_loai);
@@ -33,8 +33,6 @@ class C_san_pham
 		else
 		{
 			$ds_tat_ca_san_pham=$m_san_pham->Doc_tat_ca_san_pham();	
-=======
->>>>>>> 527178d2f96c5ca383bcd0e38eed2e6ac7a58faf
 		}
 		
 		//views
--- views/san_pham/v_right.php
+++ views/san_pham/v_right.php
@@ -1,128 +1,42 @@
 <div class="col-md-9">
 
-<<<<<<< HEAD
-<?php
-foreach($ds_tat_ca_san_pham as $sanpham)
-=======
-<?php 
-$danh_muc=array("wheycasein"=>"xml/phat_trien_co/whey_protein/wheycasein.xml",
-				"wheyconcentrate"=>"xml/phat_trien_co/whey_protein/wheyconcentrate.xml",
-				"wheyhydrolized"=>"xml/phat_trien_co/whey_protein/wheyhydrolized.xml",
-				"wheyproteinblend"=>"xml/phat_trien_co/whey_protein/wheyproteinblend.xml",
-				"wheyproteinisolate"=>"xml/phat_trien_co/whey_protein/wheyproteinisolate.xml",
-				"protein_bar"=>"xml/phat_trien_co/protein_bar.xml",
-				"bcaa"=>"xml/bcaa/bcaa.xml",
-				"mass_cao_nang_luong"=>"xml/tang_can/mass_cao_nang_luong.xml",
-				"mass_trung_nang_luong"=>"xml/tang_can/mass_trung_nang_luong.xml",
-				"creatine"=>"xml/tang_suc_manh/creatine.xml",
-				"pre-workout"=>"xml/tang_suc_manh/pre-workout.xml"
-);
-$tb_ma_loai="";
-if(isset($_GET["ma_loai"]))
-{
-	$ma_loai=$_GET["ma_loai"];
-	switch($ma_loai)
-	{
-		//mục phát triển cơ
-			//Wheyprotein
-		case 1:case 11:case 111:$path=$danh_muc["wheycasein"];
-		break;
-		case 112:$path=$danh_muc["wheyconcentrate"];
-		break;
-		case 113:$path=$danh_muc["wheyhydrolized"];
-		break;
-		case 114:$path=$danh_muc["wheyproteinblend"];
-		break;
-		case 115:$path=$danh_muc["wheyproteinisolate"];
-		break;
-			/**End wheyprotein**/
-		case 12:$path=$danh_muc["protein_bar"];
-		break;		
-		/******End phát triển cơ****/
-		
-		//Mục tăng cân
-		case 2:case 21:$path=$danh_muc["mass_cao_nang_luong"];
-		break;
-		case 22:$path=$danh_muc["mass_trung_nang_luong"];
-		break;
-		/***END tăng cân***/
-		
-		//Mục tăng sức mạnh
-		case 3:case 31:$path=$danh_muc["pre-workout"];
-		break;
-		case 32:$path=$danh_muc["creatine"];
-		break;
-		/***END tăng sức mạnh***/
-		
-		//Mục bcaa
-		case 5:$path=$danh_muc["bcaa"];
-		break;
-		/***END bcaa***/
-	}
-	
-	$tb_ma_loai="&ma_loai=$ma_loai";
-$ds_san_pham=simplexml_load_file($path);
-}
-else
-{
-$path=$danh_muc["wheycasein"];
-$ds_san_pham=simplexml_load_file($path);
-}
-//echo "<pre>";
-//print_r($ds_san_pham);
-?>
 
 <?php
-foreach($ds_san_pham->sp as $sanpham)
->>>>>>> 527178d2f96c5ca383bcd0e38eed2e6ac7a58faf
+foreach($ds_tat_ca_san_pham as $sanpham)
 {
 ?>
-                <div class="col-md-3 col-sm-6">
+                
+         <div class="col-md-3 col-sm-6">
                     <div class="single-shop-product">
                         <div class="product-upper">
-<<<<<<< HEAD
-                        <a href="chi_tiet_san_pham.php?ma_san_pham=<?php echo $sanpham->ma_san_pham;?>" title="<?php echo $sanpham->ten_san_pham; ?>" id="tensp<?php echo $sanpham->ma_san_pham;?>">
-                            <img src="public/layout/images/<?php echo $sanpham->hinh?>" alt=""></a>
+                        	<a href="chi_tiet_san_pham.php?ma_san_pham=<?php echo $sanpham->ma_san_pham;?>" title="<?php echo $sanpham->ten_san_pham; ?>" id="tensp<?php echo $sanpham->ma_san_pham?>">
+                            <img src="public/layout/images/<?php echo $sanpham->hinh?>" alt="">
+                            </a>
                         </div>
-                        <h2><a href="chi_tiet_san_pham.php?ma_san_pham=<?php echo $sanpham->ma_san_pham;?>" title="<?php echo $sanpham->ten_san_pham; ?>" id="tensp<?php echo $sanpham->ma_san_pham;?>"><?php echo $sanpham->ten_san_pham ?></a></h2>
+                        <h2><a href="chi_tiet_san_pham.php?ma_san_pham=<?php echo $sanpham->ma_san_pham;?>" title="<?php echo $sanpham->ten_san_pham; ?>" id="tensp<?php echo $sanpham->ma_san_pham?>"><?php echo $sanpham->ten_san_pham; ?></a></h2>
                         <div class="product-carousel-price">
                             <ins><?php echo $sanpham->don_gia?></ins>
-                            <input type="hidden" id="dongia<?php echo $sanpham->ma_san_pham?>" value="<?php echo $sanpham->gia?>" />
+                            <input type="hidden" id="dongia<?php echo $sanpham->ma_san_pham?>" value="<?php echo $sanpham->don_gia?>" />
                             <input type="hidden" id="hinh<?php echo $sanpham->ma_san_pham?>" value="<?php echo $sanpham->hinh?>" />
-                            <input type="hidden" id="soluong<?php echo $sanpham->ma_san_pham;?>" value="1" />
+                            <input type="hidden" id="soluong<?php echo $sanpham->ma_san_pham?>" value="1" />
                         </div>  
                         
                         <div class="product-option-shop">
                             <a class="add_to_cart_button" data-quantity="1" data-product_sku="" data-product_id="70" rel="nofollow" href="javascript:XL_ajax(<?php echo $sanpham->ma_san_pham?>)" onclick="window.location.reload(true)">Thêm vào giỏ</a>
-=======
-                            <img src="public/images/<?php echo $sanpham->hinh?>" alt="">
-                        </div>
-                        <h2><a href="chitietsanpham.php?id=<?php echo $sanpham->id;?><?php echo $tb_ma_loai ?>" title="<?php echo $sanpham->ten_sp; ?>" id="tensp<?php echo $sanpham->id?>"><?php echo $sanpham->ten_sp ?></a></h2>
-                        <div class="product-carousel-price">
-                            <ins><?php echo $sanpham->gia?></ins>
-                            <input type="hidden" id="dongia<?php echo $sanpham->id?>" value="<?php echo $sanpham->gia?>" />
-                            <input type="hidden" id="hinh<?php echo $sanpham->id?>" value="<?php echo $sanpham->hinh?>" />
-                            <input type="hidden" id="soluong<?php echo $sanpham->id?>" value="1" />
-                        </div>  
-                        
-                        <div class="product-option-shop">
-                            <a class="add_to_cart_button" data-quantity="1" data-product_sku="" data-product_id="70" rel="nofollow" href="javascript:XL_ajax(<?php echo $sanpham->id?>)" onclick="window.location.reload(true)">Thêm vào giỏ</a>
->>>>>>> 527178d2f96c5ca383bcd0e38eed2e6ac7a58faf
                         </div>                      
                     </div>
-                </div>
-                
+                </div>  
 <?php 
 }
-<<<<<<< HEAD
+
 ?>                                                    
-=======
+
 
 
 ?>  
                 
                 
                        
->>>>>>> 527178d2f96c5ca383bcd0e38eed2e6ac7a58faf
+
                 
 </div>
