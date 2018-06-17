dbc97c8ab490d131bf07a07b0dcaadc27d277381
dangnghia1997 <dangnghia25197@gmail.com>
2018-06-17 13:59:14 +0700

    Plugin pure Chat

--- templates/footer-top.php
+++ templates/footer-top.php
@@ -1,3 +1,5 @@
+
+<script type='text/javascript' data-cfasync='false'>window.purechatApi = { l: [], t: [], on: function () { this.l.push(arguments); } }; (function () { var done = false; var script = document.createElement('script'); script.async = true; script.type = 'text/javascript'; script.src = 'https://app.purechat.com/VisitorWidget/WidgetScript'; document.getElementsByTagName('HEAD').item(0).appendChild(script); script.onreadystatechange = script.onload = function (e) { if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) { var w = new PCWidget({c: '5638207d-1a11-4571-bb8f-6993fa1b75f9', f: true }); done = true; } }; })();</script>
 <a href="#" class="scrollToTop"><i class="fa fa-chevron-up"></i></a>
 <div class="footer-top-area">
         <div class="zigzag-bottom"></div>
