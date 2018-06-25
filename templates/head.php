c96dae85cc965d788ffa3ab3b177be173a1f5247
dangnghia1997 <dangnghia25197@gmail.com>
2018-06-26 01:54:11 +0700

    Cap nhat va hoan tat website

--- templates/head.php
+++ templates/head.php
@@ -1,21 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
-  <head>
+<html lang="en"><head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>eFood Supplements</title>
     
     <!-- Google Fonts -->
-    <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,200,300,700,600' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Raleway:400,100' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="public/layout/css/css1.css">
+    <link rel="stylesheet" href="public/layout/css/css2.css">
+    <link rel="stylesheet" href="public/layout/css/css3.css">
     
     <!-- Bootstrap -->
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="public/layout/css/bootstrap.min.css">
     
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="public/layout/css/font-awesome.min.css">
     
     
     <!-- Latest jQuery form server -->
@@ -45,7 +44,7 @@
     <link rel="stylesheet" href="public/layout/css/owl.carousel.css">
     <link rel="stylesheet" href="public/layout/css/style.css">
     <link rel="stylesheet" href="public/layout/css/responsive.css">
-    <link rel="stylesheet" href="public/layout/css/font-awesome.min.css">
+    
 	<link rel="stylesheet" href="public/layout/css/jquery-ui.css">
     
     
